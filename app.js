5a52f88129e2c922ad5b33028b0c4209d8181d36
Rama Krishna Rao K <87325307+kakumaniramakrishnarao@users.noreply.github.com>
2022-08-23 14:23:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,9 +87,6 @@ app.post('/checkQ',(req,res)=>{
 
 
 
-
-
-
-app.listen(3000,()=>{
-    console.log("server is running");
-})
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000.");
+});
